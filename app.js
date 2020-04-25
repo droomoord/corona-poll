c2154208aa67cfc5a47114de3e30f1714dd5ab26
droomoord <krizheijnen@gmail.com>
2020-04-25 14:38:44 +0200

    get ready for deploy

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("config");
 const path = require("path");
+const bcrypt = require("bcrypt");
+
+const PollData = require("./models/pollData");
 
 app.use(express.json());
 
@@ -22,12 +25,89 @@ const connectDB = async () => {
 
 connectDB();
 
+//get the number of datasets plus the content of all data:
+
+app.get("/polldata", async (req, res) => {
+  try {
+    const pollData = await PollData.find();
+    const numberOfDataSets = pollData.length;
+    res.json({
+      numberOfDataSets,
+      pollData,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/polldata/question/:question", async (req, res) => {
+  try {
+    const pollData = await PollData.find();
+    let answers = [];
+    pollData.forEach((dataSet) => {
+      answers.push(dataSet.userInput[req.params.question]);
+    });
+    res.json({ requestedQuestion: req.params.question, answers });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/polldata/question/:question/answer/:answer", async (req, res) => {
+  try {
+    const pollData = await PollData.find();
+    let answers = [];
+    pollData.forEach((dataSet) => {
+      answers.push(dataSet.userInput[req.params.question]);
+    });
+    const numberOfAnswersPresent = answers.filter(
+      (answer) => answer == req.params.answer
+    ).length;
+    const percentage =
+      Math.round((numberOfAnswersPresent / answers.length) * 10000) / 100;
+    res.json({ numberOfAnswersPresent, percentage, answers });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/polldata", (req, res) => {
+  const reqData = req.body;
+  bcrypt.hash(reqData.email, config.get("salt"), (err, hash) => {
+    if (!err) {
+      PollData.findOne({ email: hash }, (err, pollData) => {
+        if (pollData) {
+          res
+            .status(201)
+            .json({ msg: "Er is al een poll ingevuld met dit emailadres!" });
+        } else {
+          let newPollData = new PollData();
+          newPollData.userInput = reqData.pollData;
+          newPollData.email = hash;
+          newPollData.date = reqData.date;
+          newPollData.save();
+          res.status(200).json({
+            msg: "Gegevens opgeslagen...",
+          });
+        }
+      });
+    } else {
+      console.log(err);
+      res.send(
+        "Er is een error ontstaan in de server. Wees lief, en laat me dit alsjeblieft weten: heijnenkris@gmail.com"
+      );
+    }
+  });
+});
+
 app.get("/", (req, res) => {
-  res.send("success");
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res
+    .status(404)
+    .send("Oh oh... The server is not so happy with your request...");
 });
 
 const PORT = process.env.PORT || 5000;
--- models/pollData.js
+++ models/pollData.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
-
 const pollDataSchema = new mongoose.Schema({
-  userInput: Array,
+  email: {
+    type: String,
+    required: true
+  },
+  userInput: {
+    type: Array,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
 });
 
 const pollData = mongoose.model("pollData", pollDataSchema);
